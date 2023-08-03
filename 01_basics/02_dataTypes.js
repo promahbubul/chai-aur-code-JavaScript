8c522bc6c2058f4cc9eb6056215ee813700bd638
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-08-03 13:47:13 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+// alert("Hello Bangladesh"); // we are using nodejs, not browser
+
+console.log( 3 
+    +
+    3); // code readabiluty that should be high
+
+console.log("Bangladesh Mahbub")
+
+let age = 18;
+let isLogIn = false;
+let state;
+
+
+// number => 2 to power 53
+// bigInt => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
