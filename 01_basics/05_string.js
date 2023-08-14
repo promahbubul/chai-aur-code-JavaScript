436a946d1bd142f5be70c9e3dcf83f11d07e2d73
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-08-14 02:19:04 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = 'Mahbubul Alam';
+const githubRepo = 45;
+
+// console.log(name + githubRepo + " Someother");
+
+// `` => String interpolation
+// console.log(`My name is ${name} and my github repo count is ${githubRepo}`); 
+
+
+const gameName = new String('Free-fire-pubge');
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('r'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+// const newString = '        Mahbubul     ';
+// console.log(newString);
+// console.log(newString.trim());
+
+
+const url = 'https://google.com/meet%20xy-ub-nnm';
+
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('google'));
+// console.log(url.includes('mahbub'));
+
+console.log(gameName.split('-'))
--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -1,37 +1,37 @@
 // ********************* Primitive *********************
 
 // 7 types: String, Number, boolean, null, undefined, Symbol, BigInt
-const score = 100;
-const scoreValue = 103.05;
+// const score = 100;
+// const scoreValue = 103.05;
 
-const isLoggedIn = false;
-const outSideTem = null;
-let outEmail;
+// const isLoggedIn = false;
+// const outSideTem = null;
+// let outEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
-const bigNumber = 235223510254n;
-console.log(typeof bigNumber);
+// const bigNumber = 235223510254n;
+// console.log(typeof bigNumber);
 
 
 // ************* Reference (Non Primitive) *************
 
 // Array, Objects, Functions
-const friendName = ["Akbor", "Abdullah", "Muhammed", "Hassan"];
+// const friendName = ["Akbor", "Abdullah", "Muhammed", "Hassan"];
 
-let myObj = {
-    name : "Mahbubul Alam",
-    age: 21,
-    college: "Ananda Mohon College"
-}
+// let myObj = {
+//     name : "Mahbubul Alam",
+//     age: 21,
+//     college: "Ananda Mohon College"
+// }
 
-const myFunction = function() {
-    console.log("Hello Bangladesh");
-}
+// const myFunction = function() {
+//     console.log("Hello Bangladesh");
+// }
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -39,22 +39,47 @@ const myFunction = function() {
 
 // Stack (Primitive), Heap (Non Primitive)
 
-let myYoutubeName = "promahbubul";
+// let myYoutubeName = "promahbubul";
 
-let anotherName = myYoutubeName;
-anotherName = "sikhisikhai";
+// let anotherName = myYoutubeName;
+// anotherName = "sikhisikhai";
 
-console.log(anotherName);
-console.log(myYoutubeName);
+// console.log(anotherName);
+// console.log(myYoutubeName);
 
+
+// let userOne = {
+//     email : 'user@gmail.com',
+//     password: "522332Ma"
+// }
+
+// let userTwo = userOne
+// userTwo.email = 'mahbub@google.com';
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// let myCompany = 'Brabo it Ltd';
+
+// let anotherCompany =  myCompany;
+// anotherCompany = 'Alfa it Ltd'
+
+// console.log(anotherCompany);
+// console.log(myCompany);
 
 let userOne = {
-    email : 'user@gmail.com',
-    password: "522332Ma"
+    name: 'Mabubul Alam',
+    address: 'Dhaka Nelkhat',
+    phone: '01528252585'
 }
 
 let userTwo = userOne
-userTwo.email = 'mahbub@google.com';
+userTwo.name = 'Muhammad Abdullah'
 
-console.log(userOne.email);
-console.log(userTwo.email);
+console.log(userTwo.name);
+console.log(userOne.name);
